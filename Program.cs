60216d8a9a5243c0699fcbab9e35cbff255d40ff
DURAISELVAMS <41573700+DURAISELVAMS@users.noreply.github.com>
2020-01-07 16:59:35 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OSIsoft.AF;
+using OSIsoft.AF.UnitsOfMeasure;
+using OSIsoft.AF.Asset;
+using OSIsoft.AF.Data;
+using OSIsoft.AF.Search;
+using OSIsoft.AF.Time;
+using OSIsoft.AF.PI;
+
+namespace PI_Connect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                PISystems piSystems = new PISystems();
+                string AFServer = "PIAFSVR001";
+                PISystem assetServer = piSystems[AFServer];
+
+                PIServers piServers = new PIServers();
+                string PIServer = "PISVR01";
+                PIServer piServer = piServers[PIServer];
+
+                //AFElement.LoadAttributes()
+                if (assetServer == null)
+                {
+                    Console.WriteLine("Cannot Find AF Server: {0}", AFServer);
+                    Console.ReadLine();
+                    Environment.Exit(0);
+                }
+                if (piServer == null)
+                {
+                    Console.WriteLine("Cannot Find PI Data Server: {0}", PIServer);
+                    Console.ReadLine();
+                    Environment.Exit(0);
+                }
+
+                if (assetServer != null && piServer != null)
+                {
+                    //AFDatabase database = assetServer.Databases["Demo UOG Well Drilling & Completion Monitoring"];
+                    AFDatabase database = assetServer.Databases["AARA OEE Demo"];
+
+
+                    // PrintElementTemplates(database);
+                    // PrintAttributeTemplates(database, "Batch Context Template");
+                    // PrintEnergyUOMs(assetServer);
+                    // PrintEnumerationSets(database);
+                    // PrintCategories(database);
+                    // FindMetersBYTemplate(database, "Line");
+
+                    // TimeSpan test = new TimeSpan(0, 1, 0);
+                    //Program6.PrintInterpolated(database, "Inlet Pump", "14-Nov-18 16:15:00", "14-Nov-18 17:15:00.000000", test);
+                    //Program6.CreateFeedersRootElement(database);
+                    // Program6.CreateFeederElements(database);
+
+                    //DataPipeSubscribeExample.Run(piServer);
+                    Console.WriteLine("Connected Successfully to AF Server: {0} and PI Server: {1}", PIServer, AFServer);
+                    UseCase_DataPipe.Run(piServer);
+                    Console.ReadLine();
+
+                }
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+        }
+
+        static void PrintElementTemplates(AFDatabase database)
+        {
+            try
+            {
+                Console.WriteLine("Print Element Templates");
+                AFNamedCollectionList<AFElementTemplate> elemTemplates = database.ElementTemplates.FilterBy(typeof(AFElement));
+                foreach (AFElementTemplate elemTemp in elemTemplates)
+                {
+                    Console.WriteLine("Name: {0}; Categories: {1}", elemTemp.Name, elemTemp.CategoriesString);
+
+                }
+                Console.WriteLine();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+        }
+
+        static void PrintAttributeTemplates(AFDatabase database, string elemTempName)
+        {
+            try
+            {
+                Console.WriteLine("Print Attribute Templates for Element Template:     {0}", elemTempName);
+                AFElementTemplate elemTemp = database.ElementTemplates[elemTempName];
+                foreach (AFAttributeTemplate attrTemp in elemTemp.AttributeTemplates)
+                {
+                    string drName = attrTemp.DataReferencePlugIn == null ? "None" : attrTemp.DataReferencePlugIn.Name;
+                    Console.WriteLine("Name: {0}, DRPlugin: {1}", attrTemp.Name, drName);
+                }
+                Console.WriteLine();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        static void PrintEnergyUOMs(PISystem system)
+        {
+            try
+            {
+                Console.WriteLine("Print Energy UOMS");
+                UOMClass uomClass = system.UOMDatabase.UOMClasses["Specific Entropy, Specific Heat Capacity"];
+
+
+                foreach (UOM uom in uomClass.UOMs)
+                {
+                    Console.WriteLine("UOM: {0}, Abbrevation: {1}", uom.Name, uom.Abbreviation);
+                }
+                Console.WriteLine();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        static void PrintEnumerationSets(AFDatabase database)
+        {
+            try
+            {
+                Console.WriteLine("Print Enumeration Sets");
+
+                AFEnumerationSets enumSets = database.EnumerationSets;
+                foreach (AFEnumerationSet enumSet in enumSets)
+                {
+                    Console.WriteLine(enumSet.Name);
+                    foreach (AFEnumerationValue state in enumSet)
+                    {
+                        int stateValue = state.Value;
+                        string stateName = state.Name;
+                        Console.WriteLine("{0} - {1}", stateValue, stateName);
+                    }
+
+
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        static void PrintCategories(AFDatabase database)
+        {
+            try
+            {
+                Console.WriteLine("Print Categories");
+                AFCategories elemCategories = database.ElementCategories;
+                AFCategories attrCategories = database.AttributeCategories;
+
+
+                Console.WriteLine("Element Categories");
+                foreach (AFCategory category in elemCategories)
+                {
+                    Console.WriteLine("{0}", category.Name);
+
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Attribute Categories");
+                foreach (AFCategory category in attrCategories)
+                {
+                    Console.WriteLine(category.Name);
+
+                }
+                Console.WriteLine();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+        }
+
+        // Exercise 1: Find meters by template 
+        static void FindMetersBYTemplate(AFDatabase database, string templateName)
+        {
+            try
+            {
+                Console.WriteLine("Find Meters By Template: {0}", templateName);
+                using (AFElementSearch elementQuery = new AFElementSearch(database, "TemplateSearch", string.Format("template:\"{0}\"", templateName)))
+                {
+                    elementQuery.CacheTimeout = TimeSpan.FromMinutes(5);
+                    int countDerived = 0;
+                    foreach (AFElement element in elementQuery.FindElements())
+                    {
+                        Console.WriteLine("Element: {0}, Template: {1}", element.Name, element.Template);
+                        if (element.Template.Name != templateName)
+                            countDerived++;
+                    }
+                    Console.WriteLine("Found {0} derived Templates", countDerived);
+                    Console.WriteLine();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        // Exercise 3: Find meters with above-average usage: 
+
+        static void FindMetersAboveUsage(AFDatabase database, double val)
+        {
+            try
+            {
+                Console.WriteLine("Find Meters Above Usuage: {0}", val);
+                string templateName = "MeterBasic";
+                string attributeName = "Energy Usage";
+                AFElementSearch elementQuery = new AFElementSearch(database, " AttributeValueGTSearch", string.Format("template:\"{0}\" \"|{1}\":>{2}", templateName, attributeName, val));
+                int countNames = 0;
+                foreach (AFElement element in elementQuery.FindElements())
+                {
+                    Console.WriteLine("{0}{1}", countNames++ == 0 ? string.Empty : ", ", element.Name);
+                }
+                Console.WriteLine();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+
+    }
+}
---
+++ UseCase_DataPipe.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OSIsoft.AF.Asset;
+using OSIsoft.AF.Data;
+using OSIsoft.AF.PI;
+
+namespace PI_Connect
+{
+    class UseCase_DataPipe
+    {
+        public static void Run(PIServer piserver)
+        {
+            //PIDataPipe1
+            PIDataPipe piDP_A = new PIDataPipe(AFDataPipeType.TimeSeries);
+            try
+            {
+                //List of PIPoints
+                var ptListNames = new List<string> { "sinusoid", "CDT158Test" };
+                var ptList = PIPoint.FindPIPoints(piserver, ptListNames.AsEnumerable());
+
+                //Find Tags Available in the DataArchive
+                foreach (var val in ptListNames)
+                {
+                    if ((PIPoint.TryFindPIPoint(piserver, val, out PIPoint point) == false))
+                        Console.WriteLine("Tag Has been DELETED: {0}", val.ToString());
+                }
+                Console.ReadLine();
+
+                //PIDataPipe1
+                //Moved below line above try block 
+                //PIDataPipe piDP_A = new PIDataPipe(AFDataPipeType.TimeSeries);
+                var errPIa = piDP_A.AddSignups(ptList.Take(2).ToList());
+                var observerpiA = new PipeObserver("observerpiA");
+                piDP_A.Subscribe(observerpiA);
+
+                //using 3 minutes time to gracefully exiting and showing how dispose is working in finally block
+                DateTime start = DateTime.Now;
+                while (true && DateTime.Now.Subtract(start).Minutes < 3)
+                    {
+                        bool hasMorePiA;
+                        //Get updates    
+                        var PIa = piDP_A.GetObserverEvents(100, out hasMorePiA);
+                    }
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            finally
+            {
+                //close – which will terminate the data pipes connection to the PI Servers associated with the monitored PI Point Object
+                //Dispose - which will terminate the data pipes connection to the PI Servers associated with the monitored PI Point Object.This method also releases the resources used by the PIDataPipe
+
+                piDP_A.Close();
+                piDP_A.Dispose();
+            }
+
+        }
+    }
+
+
+    internal class Pipe_Observer : IObserver<AFDataPipeEvent>
+    {
+
+        public IDisposable UnSubscriber;
+        private List<AFDataPipeEvent> _pendingEvents;
+        private bool _bHasComplete;
+        private int _errorCount;
+        private string _Name;
+        public Pipe_Observer(string name)
+        {
+            try
+            {
+                _pendingEvents = new List<AFDataPipeEvent>();
+                _bHasComplete = false;
+                _errorCount = 0;
+                _Name = name;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+        // IObserver interfaces    
+        public void OnNext(AFDataPipeEvent value)
+        {
+            try
+            {
+                AFValue val = value.Value;
+                Console.WriteLine($"{val.Value.ToString()} {val.PIPoint.Name} {val.Timestamp}");
+                _pendingEvents.Add(value);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+
+        }
+        public void OnError(Exception ex)
+        {
+            _errorCount++;
+        }
+        public void OnCompleted()
+        {
+            _bHasComplete = true;
+        }
+    }
+}
